720757415a14949ab93346e52eba58e33f8122ac
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-06-24 23:46:49 -0700

    started setting up the htmlRoutes.js with the proper module export setup and wrote out the first .get method needed

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,6 @@
+module.exports = function(app, path, db) {
+    //this will set up the workout to continue or start a new one when the button is clicked from index.html
+    app.get("/exercise", function(req, res) {
+        res.sendFile(path.join(_dirname, "../public/exercise.html"));
+    });
+};
