9d86fb5094eaf76de4b03fb93421e38cc6aaf634
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-06-24 23:31:41 -0700

    added a gitignore file and ran npm install, created a routes folder with my api and html routes, begain building my apiRoutes.js file

---
+++ routes/apiRoutes.js
@@ -0,0 +1,42 @@
+module.exports = function(app, path, db) {
+
+//reaches out to the api.js file to get the last logged workout
+  app.get("/api/workouts", (req, res) => {
+      db.Workout.find({})
+      .then(workout => {
+         res.json(workout);
+      })
+      .catch(err => {
+          res.json(err);
+      });
+    });
+
+    //post method creates a new workout in the db
+    app.post("/api/workouts", async (req, res) => {
+        try {
+            const response = await db.Workout.create({type: "workout"})
+            res.json(response);
+        }
+        catch(err){
+            console.log("there was an error while creating the workout", err)
+        }
+    })
+
+    //this is where a new workout will be created by calling on the api.js
+    app.put("/api/workouts/:id", ({body, params}, res) => {
+        const workoutId = params.id;
+        let savedExercises = [];
+
+        //this will collect all the saved exercises in the workout
+        db.Workout.find({_id: workoutId})
+        .then(dbWorkout => {
+            savedExercises = dbWorkout[0].exercises;
+            res.json(dbWorkout[0].exercises);
+            let allExercises = [...savedExercises, body]
+            updateWorkout(allExercises)
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+};
