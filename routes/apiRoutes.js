6375346221b6f8733a0a907228aa9b7fff4b42ea
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-06-26 20:08:24 -0700

    made changes to the apiRoutes for functunality, took off db. on lines 6 and 16 and rewrote the .post and .put routes, everything seems to be working now

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,7 @@ const Workout = require("../models/workout");
 module.exports = function (app) {
   //reaches out to the api.js file to get the last logged workout
   app.get("/api/workouts", (req, res) => {
-    db.Workout.find()
+    Workout.find()
       .then((workoutdata) => {
         res.json(workoutdata);
       })
@@ -13,7 +13,7 @@ module.exports = function (app) {
   });
 
   app.get("/api/workouts/range", (req, res) => {
-    db.Workout.find()
+    Workout.find()
       .then((workoutdata) => {
         res.json(workoutdata);
       })
@@ -29,44 +29,26 @@ module.exports = function (app) {
         res.json(err);
       });
   });
-  
+
   //post method creates a new workout in the db
-  app.post("/api/workouts", async (req, res) => {
-    try {
-      const response = await db.Workout.create({ type: "workout" });
-      res.json(response);
-    } catch (err) {
-      console.log("there was an error while creating the workout", err);
-    }
-  });
-
-  //this is where a new workout will be created by calling on the api.js
-  app.put("/api/workouts/:id", ({ body, params }, res) => {
-    const workoutId = params.id;
-    let savedExercises = [];
-
-    //this will collect all the saved exercises in the workout
-    db.Workout.find({ _id: workoutId })
-      .then((dbWorkout) => {
-        savedExercises = dbWorkout[0].exercises;
-        res.json(dbWorkout[0].exercises);
-        let allExercises = [...savedExercises, body];
-        updateWorkout(allExercises);
-      })
+  app.post("/api/workouts", function (req, res) {
+    Workout.create({})
+      .then((workoutdata) => res.json(workoutdata))
       .catch((err) => {
         res.json(err);
       });
-    function updateWorkout(exercises) {
-      db.Workout.findByIdAndUpdate(
-        workoutId,
-        { exercises: exercises },
-        function (err, doc) {
-          if (err) {
-            console.log(err);
-          }
-        }
-      );
-    }
   });
-
+  //this is where a new workout will be created by calling on the api.js
+  app.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } },
+      { new: true, runValidators: true }
+    )
+      .then((workoutdata) => res.json(workoutdata))
+      .catch((err) => {
+        console.log("err", err);
+        res.json(err);
+      });
+  });
 };
